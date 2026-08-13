4095d0c391497349e5424f063de57604878090ee
Arda Kemiksiz <eldeirun06@gmail.com>
2026-08-13 16:40:06 +0300

    AuthcContext written

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -13,4 +13,23 @@ const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [token, setToken] = useState<string | null>(null);
   const [user, setUser] = useState<any | null>(null);
+
+  const loginUser = (token: string, user: any) => {
+    setToken(token);
+    setUser(user);
+    setAuthToken(token);
+  };
+
+  const logoutUser = () => {
+    setToken(null);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, user, loginUser, logoutUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
+
+export const useAuth = () => useContext(AuthContext);
